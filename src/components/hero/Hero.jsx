daae4cc60c1a221094a92615ce33da7e26e92f51
Mohammed Fayiz k <mhdfayizk@gmail.com>
2023-09-26 17:33:32 +0530

    corruction

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,7 +9,7 @@ import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import resumePDF from "../../assets/resume.pdf";
-import my from '../../assets/Fazz.png'
+import my from './../../assets/Fazz.png'
 
 
 const Hero = () => {
